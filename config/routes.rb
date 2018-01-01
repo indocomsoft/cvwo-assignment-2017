1790db1b4b60ac63b3a0d0fd7dd92e6906e2f910
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-01-01 22:16:47 +0700

    Added comments in route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tasks do
     member do
+      # RESTful API
+
+      # For the done checkbox
       post 'done'
     end
   end
