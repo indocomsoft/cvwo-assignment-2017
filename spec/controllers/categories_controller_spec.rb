baccee15716a5a98df6791ccacfbb1a463fe75de
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-01-03 21:54:13 +0700

    Finished CategoriesController Tests

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,66 +1,89 @@
 require 'rails_helper'
 
 RSpec.describe CategoriesController, type: :controller do
-  # it do
-  #   params = { category: { name: 'Test' } }
-  #   should permit(:name).for(:create, verb: :post, params: params).on(:category)
-  # end
+  describe "GET #index" do
+    before { get :index }
+    it { should render_template "index" }
+    it { should respond_with :ok }
+  end
+
+  describe "GET #new" do
+    before { get :new }
+    it { should render_template "new" }
+    it { should respond_with :ok }
+  end
 
   describe "POST #create" do
-    before(:each) do
-      @params = { category: { name: 'Test'} }
+    before(:each) { @params = { category: { name: 'Test'} } }
+
+    it { should permit(:name).for(:create, verb: :post, params: @params).on(:category) }
+    
+    context "given valid category" do
+      it do
+        post :create, { params: @params }
+        should redirect_to categories_path
+      end
     end
-    it do
-      should permit(:name).for(:create, verb: :get, params: @params).on(:category)
-    end
-    it "should redirect to /categories if successful" do
-      post :create, { params: @params }
-      should redirect_to categories_path
-    end
-    it "should fail if a given name already existed" do
-      Category.create(name: "Test")
-      post :create, { params: @params }
-      should respond_with :bad_request
-    end
+    
+    context "given invalid category" do
+      before(:each) do
+        Category.create(name: "Test")
+        post :create, { params: @params }
+      end
+      it { should respond_with :bad_request }
+      it { should render_template "new" }
+    end   
   end
 
   describe "GET #edit" do
-    before(:each) do
-      @category = Category.create(name: "Test")
+    before(:each) { @category = Category.create(name: "Test") }
+
+    context "given valid id" do
+      before(:each) { get :edit, params: { id: @category.id } }
+
+      it { should respond_with :ok }
+      it { should render_template "edit" }
     end
-    it do
-      get :edit, params: { id: @category.id }
-      should respond_with :ok
-    end
-    it do
-      expect {
-        get :edit, params: { id: 'unknown' }
-      }.to raise_error(ActiveRecord::RecordNotFound) 
+
+    context "given invalid id" do
+      it do
+        expect {
+          get :edit, params: { id: 'unknown' }
+        }.to raise_error(ActiveRecord::RecordNotFound) 
+      end
     end
   end
 
   describe "POST #update" do
     before(:each) do
       @category = Category.create(name: "Test")
+      @category2 = Category.create(name: "Test2")
     end
-
-  end
-
-  describe "GET #index" do
-    before { get :index }
-    it { should respond_with :ok }
-  end
-
-  describe "GET #new" do
-    before { get :new }
-    it { should respond_with :ok}
+    it do
+      should permit(:name).
+        for(:update, verb: :post, params: { id: @category.id, category: { name: "NewTest"} } ).
+        on(:category)
+    end
+    context "given valid new name" do
+      before(:each) do
+        post :update, { params: { id: @category.id, category: { name: "NewTest" } } }
+      end
+      it { should redirect_to categories_path }
+    end
+    context "given invalid new name" do
+      before(:each) do
+        post :update, { params: { id: @category.id, category: { name: "Test2" } } }
+      end
+      it { should respond_with :bad_request }
+      it { should render_template "edit" }
+    end
   end
 
   describe "DELETE #destroy" do
     before(:each) do
       @category = Category.create(name: "Test")
     end
-    it "destroys successfully" do
+    it do
       expect { 
         delete :destroy, params: { id: @category.id }
       }.to change { Category.count }.by(-1)
