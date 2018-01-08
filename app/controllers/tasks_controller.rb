52f9aafd458ba4a5935fadf4d11876cc443b9485
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-01-08 21:59:16 +0700

    Implemented colours for categories. Resolves #1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
-  helper_method :sortable
+  helper_method :sortable, :choose_fgcolour
 
   # Helper to generate table header in view
   def sortable(column, title = nil)
@@ -13,6 +13,27 @@ class TasksController < ApplicationController
                   "asc"
                 end
     view_context.link_to title.html_safe, {sort: column, direction: direction}
+  end
+
+  # Helper to choose foreground colour from a given background colour, following W3C Recommendation
+  # https://www.w3.org/TR/WCAG20/#relativeluminancedef
+  def choose_fgcolour(bgcolour)
+    # Extract the red, green, blue components from the colour hexcode
+    # then convert them according to the W3C recommendation 
+    r,g,b = bgcolour.match(/#(..)(..)(..)/).to_a.drop(1).map do |e|
+      a = e.hex / 255
+      if a <= 0.03928
+        a / 12.92
+      else
+        ((a + 0.055) / 1.055) ** 2.4
+      end
+    end
+    luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b
+    if luminance > 0.179
+      "#000000"
+    else
+      "#FFFFFF"
+    end
   end
 
   def index
