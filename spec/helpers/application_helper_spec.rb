57ad32e4dd201f909ad8a23dc0d753ab1866a544
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-01-08 22:23:47 +0700

    Added test for choose_fgcolour

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,24 @@
+require "spec_helper"
+
+describe ApplicationHelper do
+  describe "#choose_fgcolour" do
+    it "given black should return white" do
+      expect(helper.choose_fgcolour("#000000")).to eq("#FFFFFF")
+    end
+    it "given white should return black" do
+      expect(helper.choose_fgcolour("#FFFFFF")).to eq("#000000")
+    end
+    it "given blue should return white" do
+      expect(helper.choose_fgcolour("#0000FF")).to eq("#FFFFFF")
+    end
+    it "given red should return black" do
+      expect(helper.choose_fgcolour("#FF0000")).to eq("#000000")
+    end
+    it "given green should return black" do
+      expect(helper.choose_fgcolour("#00FF00")).to eq("#000000")
+    end
+    it "given #BADA55 should return white" do
+      expect(helper.choose_fgcolour("#BADA55")).to eq("#FFFFFF")
+    end
+  end
+end
