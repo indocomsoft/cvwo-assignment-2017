3b85803b21838b2899dc847df3fef449b8294a57
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-01-08 22:07:30 +0700

    Refactored helper methods

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,29 +1,6 @@
 # frozen_string_literal: true
 
 class CategoriesController < ApplicationController
-  helper_method :choose_fgcolour
-
-  # Helper to choose foreground colour from a given background colour, following W3C Recommendation
-  # https://www.w3.org/TR/WCAG20/#relativeluminancedef
-  def choose_fgcolour(bgcolour)
-    # Extract the red, green, blue components from the colour hexcode
-    # then convert them according to the W3C recommendation 
-    r,g,b = bgcolour.match(/#(..)(..)(..)/).to_a.drop(1).map do |e|
-      a = e.hex / 255
-      if a <= 0.03928
-        a / 12.92
-      else
-        ((a + 0.055) / 1.055) ** 2.4
-      end
-    end
-    luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b
-    if luminance > 0.179
-      "#000000"
-    else
-      "#FFFFFF"
-    end
-  end
-
   def index
     @categories = Category.all
   end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,41 +1,6 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
-  helper_method :sortable, :choose_fgcolour
-
-  # Helper to generate table header in view
-  def sortable(column, title = nil)
-    title ||= column.titleize
-    title += "<i class=\"fa fa-sort-#{sort_direction}\"></i>" if column == sort_column
-    direction = if column == sort_column && sort_direction == "asc" 
-                  "desc"
-                else
-                  "asc"
-                end
-    view_context.link_to title.html_safe, {sort: column, direction: direction}
-  end
-
-  # Helper to choose foreground colour from a given background colour, following W3C Recommendation
-  # https://www.w3.org/TR/WCAG20/#relativeluminancedef
-  def choose_fgcolour(bgcolour)
-    # Extract the red, green, blue components from the colour hexcode
-    # then convert them according to the W3C recommendation 
-    r,g,b = bgcolour.match(/#(..)(..)(..)/).to_a.drop(1).map do |e|
-      a = e.hex / 255
-      if a <= 0.03928
-        a / 12.92
-      else
-        ((a + 0.055) / 1.055) ** 2.4
-      end
-    end
-    luminance = 0.2126 * r + 0.7152 * g + 0.0722 * b
-    if luminance > 0.179
-      "#000000"
-    else
-      "#FFFFFF"
-    end
-  end
-
   def index
     @tasks = Task.order(sort_column + " " + sort_direction)
   end
@@ -91,16 +56,6 @@ class TasksController < ApplicationController
     params.require(:task).permit(:name, :description, :priority, :done, :due_date)
   end
 
-  # Extract the column to sort by. Else, priority is the default column sorting mode
-  def sort_column
-    Task.column_names.include?(params[:sort])? params[:sort] : "priority"
-  end
-
-  # Extract the direction to sort by. Else, ascending is the default direction
-  def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
-  end
-
   # Commit the categories for a given task into the database
   def commit_categories(task, categories)
     existing = task.categories.all.map { |c| c.name }
@@ -118,4 +73,28 @@ class TasksController < ApplicationController
       Taskcategory.find_by(task: task, category: category).destroy
     end
   end
+
+  # Extract the column to sort by. Else, priority is the default column sorting mode
+  def sort_column
+    Task.column_names.include?(params[:sort])? params[:sort] : "priority"
+  end
+
+  # Extract the direction to sort by. Else, ascending is the default direction
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
+  # Helper to generate table header in view
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    title += "<i class=\"fa fa-sort-#{sort_direction}\"></i>" if column == sort_column
+    direction = if column == sort_column && sort_direction == "asc" 
+                  "desc"
+                else
+                  "asc"
+                end
+    view_context.link_to title.html_safe, {sort: column, direction: direction}
+  end
+
+  helper_method :sortable
 end
