5f9b137c95fbf92b05a8747897ecd3bbc4e2c3b3
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-01-10 11:13:04 +0700

    Added more tests

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -31,25 +31,62 @@ RSpec.describe CategoriesController, type: :controller do
   end
 
   describe "POST #create" do
-    before(:each) { @params = { category: { name: 'Test'}, task: '' } }
-
-    it { should permit(:name).for(:create, verb: :post, params: @params).on(:category) }
+    it do
+      should permit(:name).
+        for(:create, verb: :post, params: { category: { name: 'Test'}, task: '' } ).
+        on(:category)
+    end
     
     context "given valid category" do
-      it do
-        post :create, { params: @params }
-        should redirect_to categories_path
+      context "given no task" do
+          before(:each) { @params = { category: { name: 'Test'}, task: '' } }
+        it do
+          post :create, { params: { category: { name: 'Test'}, task: '' } }
+          should redirect_to categories_path
+        end
+      end
+      context "given one task" do
+        before(:each) do
+          @task = Task.create(name: 'Test', priority: 1)
+        end
+        it do
+          post :create, { params: { category: { name: 'asd' }, task: 'Test' } }
+          should redirect_to categories_path
+        end
+        it do
+          expect {
+            post :create, { params: { category: { name: 'asd' }, task: 'Test' } }
+          }.to change { @task.categories.count }.by(1)
+           .and change { Category.all.count }.by(1)
+        end
+      end
+      context "given multiple tasks" do
+        before(:each) do
+          @task1 = Task.create(name: 'Test1', priority: 1)
+          @task2 = Task.create(name: 'Test2', priority: 2)
+        end
+        it do
+          post :create, { params: { category: { name: 'asd' }, task: 'Test1,Test2' } }
+          should redirect_to categories_path
+        end
+        it do
+          expect {
+            post :create, { params: { category: { name: 'asd' }, task: 'Test1,Test2' } }
+          }.to change { @task1.categories.count }.by(1)
+           .and change { @task2.categories.count }.by(1)
+           .and change { Category.all.count }.by(1)
+         end
       end
     end
     
     context "given invalid category" do
       before(:each) do
         Category.create(name: "Test")
-        post :create, { params: @params }
+        post :create, { params: { category: { name: 'Test'}, task: '' } }
       end
       it { should respond_with :bad_request }
       it { should render_template "new" }
-    end   
+    end 
   end
 
   describe "GET #edit" do
@@ -82,10 +119,82 @@ RSpec.describe CategoriesController, type: :controller do
         on(:category)
     end
     context "given valid new name" do
-      before(:each) do
-        post :update, { params: { id: @category.id, category: { name: "NewTest" }, task: '' } }
+      context "given no task" do
+        before(:each) do
+          post :update, { params: { id: @category.id, category: { name: "NewTest" }, task: '' } }
+        end
+        it { should redirect_to categories_path }
       end
-      it { should redirect_to categories_path }
+      context "given one task" do
+        before(:each) do
+          @task = Task.create(name: 'Task', priority: 1)
+        end
+        it do
+          post :update, { params: { id: @category.id, category: { name: "NewTest" }, task: 'Task' } }
+          should redirect_to categories_path
+        end
+        it do
+          expect {
+            post :update, { params: { id: @category.id, category: { name: "NewTest" }, task: 'Task' } }
+          }.to change { @category.tasks.count }.by(1)
+           .and change { @task.categories.count }.by(1)
+        end
+      end
+      context "given multiple tasks" do
+        before(:each) do
+          @task1 = Task.create(name: 'Task1', priority: 1)
+          @task2 = Task.create(name: 'Task2', priority: 2)
+        end
+        it do
+          post :update, { params: { id: @category.id, category: { name: "NewTest" }, task: 'Task1,Task2' } }
+          should redirect_to categories_path
+        end
+        it do
+          expect {
+            post :update, { params: { id: @category.id, category: { name: "NewTest" }, task: 'Task1,Task2' } }
+          }.to change { @category.tasks.count }.by(2)
+           .and change { @task1.categories.count }.by(1)
+           .and change { @task2.categories.count }.by(1)
+        end
+      end
+      context "given removal of one already assigned task" do
+        before(:each) do
+          @task1 = Task.create(name: 'Task1', priority: 1)
+          @task2 = Task.create(name: 'Task2', priority: 2)
+          Taskcategory.create(task: @task1, category: @category)
+          Taskcategory.create(task: @task2, category: @category)
+        end
+        it do
+          post :update, { params: { id: @category.id, category: { name: "NewTest" }, task: 'Task1' } }
+          should redirect_to categories_path
+        end
+        it do
+          expect {
+            post :update, { params: { id: @category.id, category: { name: "NewTest" }, task: 'Task1' } }
+            }.to change { @category.tasks.count }.by(-1)
+             .and change { @task1.categories.count }.by(0)
+             .and change { @task2.categories.count }.by(-1)
+         end
+      end
+      context "given removal of multiple already assigned tasks" do
+        before(:each) do
+          @task1 = Task.create(name: 'Task1', priority: 1)
+          @task2 = Task.create(name: 'Task2', priority: 2)
+          Taskcategory.create(task: @task1, category: @category)
+          Taskcategory.create(task: @task2, category: @category)
+        end
+        it do
+          post :update, { params: { id: @category.id, category: { name: "NewTest" }, task: '' } }
+          should redirect_to categories_path
+        end
+        it do
+          expect {
+            post :update, { params: { id: @category.id, category: { name: "NewTest" }, task: '' } }
+            }.to change { @category.tasks.count }.by(-2)
+             .and change { @task1.categories.count }.by(-1)
+             .and change { @task2.categories.count }.by(-1)
+         end
+      end
     end
     context "given invalid new name" do
       before(:each) do
