c54ff79ce635317c7becd702d8a7242bc12b9327
Julius Putra Tanu Setiaji <indocomsoft@gmail.com>
2018-01-22 17:38:58 +0800

    Finished sign-up logic and tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,28 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
+  def show
+    @user = User.find params[:id]
+  end
+
   def new
     @user = User.new
   end
+
+  def create
+    user = User.new user_params
+    if user.save
+      redirect_to user
+    else
+      @user = user
+      flash[:error] = user.errors.full_messages.join(", ")
+      render "new", status: :bad_request
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:email, :name, :password, :password_confirmation)
+    end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,41 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.describe UsersController, type: :controller do
+  describe "GET #show" do
+    before(:each) do
+      @user = User.create(email: "anu@abc.com", password: "123456")
+      get :show, params: { id: @user.id }
+    end
+    it { should respond_with :ok }
+    it { should render_template "show" }
+  end
+
+  describe "GET #new" do
+    before { get :new }
+    it { should render_template "new" }
+    it { should respond_with :ok }
+  end
+
+  describe "POST #create" do
+    it do
+      should permit(:email, :name, :password, :password_confirmation).
+        for(:create, verb: :post, params: { user: { email: "anu@abc.com", password: "123456", password_confirmation: "123456" } }).
+        on(:user)
+    end
+    it "given valid input" do
+      post :create, params: { user: { email: "anu@abc.com", password: "123456", password_confirmation: "123456" } }
+      user = User.find_by(email: "anu@abc.com")
+      should redirect_to user
+    end
+    context "given invalid input" do
+      before(:each) do
+        post :create, params: { user: { email: "anu" } }
+      end
+      it { should respond_with :bad_request }
+      it { should render_template "new" }
+    end
+  end
 
 end
